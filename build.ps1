f41c1b711c296337d0aad3efc6fc652c235146ce
James Woolfenden <james.woolfenden@gmail.com>
2022-08-17 17:08:54 +0100

    fix path

--- build.ps1
+++ build.ps1
@@ -1,8 +1,14 @@
+$ErrorActionPreference = "Stop"
 $outfile="pike.zip"
 $version=(Invoke-Webrequest https://api.github.com/repos/jameswoolfenden/pike/releases/latest|convertfrom-json).name
 
 Write-Host "$(get-date) - downloading release $version"
-Invoke-WebRequest -uri "https://github.com/jameswoolfenden/pike/releases/download/$($version)/pike-$($version)-windows-amd64.zip" -OutFile $outfile
+
+$version=$version.Replace("v","")
+
+Write-Output "https://github.com/JamesWoolfenden/pike/releases/download/v$($version)/pike_$($version)_windows_amd64.zip"
+     
+Invoke-WebRequest -uri "https://github.com/JamesWoolfenden/pike/releases/download/v$($version)/pike_$($version)_windows_amd64.zip" -OutFile $outfile
 tar -xvf $outfile -C .\tools\
 
 Write-Host "$(get-date) - packing"
