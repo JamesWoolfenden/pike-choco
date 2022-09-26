a75c9c2cddf19dc485f2dbc9b2cf8f24f3f327eb
James Woolfenden <james.woolfenden@gmail.com>
2022-09-26 10:37:56 +0100

    auto VERIFICATION.txt

--- build.ps1
+++ build.ps1
@@ -1,9 +1,10 @@
+#!/usr/bin/env pwsh
 $ErrorActionPreference = "Stop"
 $outfile="pike.zip"
 $version=(Invoke-Webrequest https://api.github.com/repos/jameswoolfenden/pike/releases/latest|convertfrom-json).name
 
 Write-Host "$(get-date) - downloading release $version"
-
+set-content -path ./tools/VERIFICATION.txt -value ((Get-Content -path ./tools/VERIFICATION.txt -Raw) -replace 'PLACEHOLDER', $version)
 $version=$version.Replace("v","")
 
 Write-Output "https://github.com/JamesWoolfenden/pike/releases/download/v$($version)/pike_$($version)_windows_amd64.zip"
